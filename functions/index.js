2995fcbf47059a77f3f184f72a258ca689e568a5
machado001 <machadowg@gmail.com>
2025-11-09 13:17:06 -0300

    Remove useless headers

--- functions/index.js
+++ functions/index.js
@@ -64,13 +64,6 @@ exports.championRotation = onRequest({
         "https://br1.api.riotgames.com/lol/platform/v3/champion-rotations",
         {
           headers: {
-            "User-Agent":
-              "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 " +
-              "(KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36",
-            "Accept-Language": "en-US,en;q=0.9,pt-BR;q=0.8,pt;q=0.7",
-            "Accept-Charset":
-              "application/x-www-form-urlencoded; charset=UTF-8",
-            "Origin": "https://developer.riotgames.com",
             "X-Riot-Token": riotApiKey,
           },
         },
